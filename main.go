7b2873982f3dadf49773a0728343c4281cd74684
Zach Holcomb <zachholcombmusic@gmail.com>
2020-09-15 19:43:11 -0600

    Adds cafes route and db connection

--- main.go
+++ main.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/jtrtj/javaworld/models"
 )
 
 func main() {
 	router := gin.Default()
+	models.ConnectDataBase()
+
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+
+	router.GET("/cafes", func(c *gin.Context) {
+		var cafes []models.Cafe
+		models.DB.Find(&cafes)
+
+		c.JSON(http.StatusOK, gin.H{"data": cafes})
 	})
 
 	router.Run()
