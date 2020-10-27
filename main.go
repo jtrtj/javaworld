4dfd35049c90a7d9e5e8131d96da278b67320feb
Zach Holcomb <zachholcombmusic@gmail.com>
2020-10-27 10:58:45 -0600

    Removes routes from main

--- main.go
+++ main.go
@@ -3,61 +3,18 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/jtrtj/javaworld/models"
-	"github.com/jtrtj/javaworld/services"
 	"net/http"
-	"net/url"
-	"io/ioutil"
-	"log"
-	"encoding/json"
-	"os"
 )
 
 func main() {
 	router := gin.Default()
 	models.ConnectDataBase()
+	models.SeedDatabase()
 
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
-		})
-	})
-
-	router.GET("/api_test", func(c *gin.Context) {
-		base, err := url.Parse("https://maps.googleapis.com/maps/api/place/")
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		// Path params
-		base.Path += "nearbysearch/json"
-
-		// Query params
-		params := url.Values{}
-		params.Add("key", os.Getenv("GOOGLE_KEY"))
-		params.Add("location", "39.7392,-104.9903" )
-		params.Add("radius", "33000" )
-		params.Add("type", "cafe" )
-		base.RawQuery = params.Encode()
-
-		resp, err := http.Get(base.String())
-
-		if err != nil {
-			log.Fatalln(err)
-		}
-
-		defer resp.Body.Close()
-		bodyBytes, _ := ioutil.ReadAll(resp.Body)
-
-		bodyString := string(bodyBytes)
-
-		var placesStruct services.PlacesResponse
-		json.Unmarshal(bodyBytes, &placesStruct)
-
-		c.JSON(200, gin.H{
-			"string_version": bodyString,
-			"json_version": placesStruct,
 		})
 	})
 
