35b94b066d8afd2077e7a9e43e31988dc5be53bc
Zach Holcomb <zachholcombmusic@gmail.com>
2020-11-03 10:19:44 -0700

    Adds updated cafe fields to seed database function and filters out Starbucks

--- models/setup.go
+++ models/setup.go
@@ -28,7 +28,16 @@ func SeedDatabase() {
 	api_call := services.Call()
 	results := api_call.Results
 	for _, input := range results {
-		cafe := Cafe{Name: input.Name, Address: input.Vicinity}
+		if ( input.Name == "Starbucks" || input.Name == "Peet's Coffee" ) {
+			continue
+		}
+		cafe := Cafe{
+								 Name: input.Name, 
+								 Address: input.Vicinity,
+								 GoogleRating: input.Rating,
+								 Lat: input.Geometry.Location.Lat,
+								 Lng: input.Geometry.Location.Lng,
+								}
 		DB.Create(&cafe)
 	}
 	check(api_call)
@@ -40,9 +49,71 @@ func check(results services.PlacesResponse) {
 		api_call := services.CallNextPage(results.NextPageToken)
 		results := api_call.Results
 		for _, input := range results {
-			cafe := Cafe{Name: input.Name, Address: input.Vicinity}
+			if ( input.Name == "Starbucks" || input.Name == "Peet's Coffee" ) {
+				continue
+			}
+			cafe := Cafe{
+								 Name: input.Name, 
+								 Address: input.Vicinity,
+								 GoogleRating: input.Rating,
+								 Lat: input.Geometry.Location.Lat,
+								 Lng: input.Geometry.Location.Lng,
+								}
 			DB.Create(&cafe)
 		}
 		check(api_call)
 	}
 }
+
+
+// {
+//             "business_status": "OPERATIONAL",
+//             "geometry": {
+//                 "location": {
+//                     "lat": 39.7718444,
+//                     "lng": -105.0441417
+//                 },
+//                 "viewport": {
+//                     "northeast": {
+//                         "lat": 39.77319467989272,
+//                         "lng": -105.0427253201073
+//                     },
+//                     "southwest": {
+//                         "lat": 39.77049502010728,
+//                         "lng": -105.0454249798927
+//                     }
+//                 }
+//             },
+//             "icon": "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/cafe-71.png",
+//             "name": "Downpours Coffee",
+//             "opening_hours": {
+//                 "open_now": true
+//             },
+//             "photos": [
+//                 {
+//                     "height": 3036,
+//                     "html_attributions": [
+//                         "<a href=\"https://maps.google.com/maps/contrib/101175743725300481004\">Anna Arenz</a>"
+//                     ],
+//                     "photo_reference": "CmRaAAAA9IQd8ePhdFmog8r-cVzm6gkzpxwBHnUYd0BnNiZKIaIEzvt_q2grZ-kkrJI3VjR-aRhm5d6nAgEXqwCLmi_WZA2QfbNQxJrD6TSsgE_wmBE2tFcIEB54t6ABVsA7VNoKEhCfqZuBHDabW_PuVbMzDySNGhRc8UG8fMF7HRJ4HPoQTOyL9OeSYQ",
+//                     "width": 4048
+//                 }
+//             ],
+//             "place_id": "ChIJHXrrTJ2Ha4cRfXJ6JkGZoXM",
+//             "plus_code": {
+//                 "compound_code": "QXC4+P8 Denver, Colorado",
+//                 "global_code": "85FPQXC4+P8"
+//             },
+//             "rating": 4.6,
+//             "reference": "ChIJHXrrTJ2Ha4cRfXJ6JkGZoXM",
+//             "scope": "GOOGLE",
+//             "types": [
+//                 "cafe",
+//                 "food",
+//                 "point_of_interest",
+//                 "store",
+//                 "establishment"
+//             ],
+//             "user_ratings_total": 173,
+//             "vicinity": "3937 Tennyson St, Denver"
+// },
